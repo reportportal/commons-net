91bf52fb2852af3778faeba0703b3ee00fc5d151
Nikolay Borisenko <nikolay.v.borisenko@gmail.com>
2022-10-05 18:22:07 +0300

    Create LogMessageExtensionsFixture.cs

---
+++ test/ReportPortal.Shared.Tests/Execution/LogMessageExtensionsFixture.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using ReportPortal.Client.Abstractions.Models;
+using ReportPortal.Shared.Execution.Logging;
+using System;
+using Xunit;
+
+namespace ReportPortal.Shared.Tests.Execution
+{
+    public class LogMessageExtensionsFixture
+    {
+        [Fact]
+        public void ShouldNotConvertNullableLogMessage()
+        {
+            ILogMessage logMessage = null;
+
+            Action act = () => logMessage.ConvertToRequest();
+
+            act.Should().ThrowExactly<ArgumentNullException>();
+        }
+
+        [Theory]
+        [InlineData(LogMessageLevel.Info, LogLevel.Info)]
+        [InlineData(LogMessageLevel.Warning, LogLevel.Warning)]
+        [InlineData(LogMessageLevel.Debug, LogLevel.Debug)]
+        [InlineData(LogMessageLevel.Trace, LogLevel.Trace)]
+        [InlineData(LogMessageLevel.Error, LogLevel.Error)]
+        [InlineData(LogMessageLevel.Fatal, LogLevel.Fatal)]
+        public void ShouldConvertLogMessage(LogMessageLevel level, LogLevel expectedLevel)
+        {
+            ILogMessage logMessage = new LogMessage("message") { Level = level};
+
+            var request = logMessage.ConvertToRequest();
+
+            request.Text.Should().Be("message");
+            request.Level.Should().Be(expectedLevel);
+        }
+    }
+}
